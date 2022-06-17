c2a387882ca342b7256a357d87d08a72b936f7be
ddawidroch <david.roch.2001@gmail.com>
2022-06-17 17:40:39 +0200

    delete activities

--- pages/homepage.js
+++ pages/homepage.js
@@ -1,12 +1,10 @@
 import Nav from '../components/nav'
-import Activity from '../components/activity'
 
 const HomePage = () => {
   return (
     <div className='flex'>
       <Nav />
       <div className='w-full'>{/* Start coding here */}</div>
-      <Activity />
     </div>
   )
 }
