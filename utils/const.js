368f3ae4750d8b000a3a5cdfd0d95b6ee219fd03
ddawidroch <david.roch.2001@gmail.com>
2022-06-17 17:40:06 +0200

    add constants to project

---
+++ utils/const.js
@@ -0,0 +1,10 @@
+import { PublicKey } from "@solana/web3.js";
+import spotifyWeb3 from "./spotify_web3.json";
+
+export const SOLANA_HOST =
+  "https://muddy-shy-dawn.solana-devnet.quiknode.pro/5b638e723dd3df716748cb5432458a2e21e53e05/";
+
+export const STABLE_POOL_PROGRAM_ID = new PublicKey(
+  "Cb5Fiou3UfNRMHJd5ry5CUAQeSGJNJaZUPwN5hM3ebUM"
+);
+export const STABLE_POOL_IDL = spotifyWeb3;
