c8a523c9fbabb4c28b036795f6b47895f4299e31
ddawidroch <david.roch.2001@gmail.com>
2022-06-18 04:11:55 +0200

    create homepage component

--- pages/homepage.js
+++ pages/homepage.js
@@ -1,12 +1,45 @@
-import Nav from '../components/nav'
+import Nav from "../components/nav";
+import { useState } from "react";
+import Header from "../components/Header";
+import useSpotify from "../hooks/useSpotify";
+import UploadModal from "../components/UploadModal";
+import Playlist from "../components/Playlist";
+import PlayerControls from "../components/PlayerControls";
+import { songs } from "../data/songs.js";
 
 const HomePage = () => {
-  return (
-    <div className='flex'>
-      <Nav />
-      <div className='w-full'>{/* Start coding here */}</div>
-    </div>
-  )
-}
+  const [showUploadMusic, setShowUploadMusic] = useState(false);
+  const [title, setTitle] = useState("");
+  const [musicUrl, setMusicUrl] = useState("");
+  //const [songs, setSongs] = useState([]);
+  const { newMusic, getSongs } = useSpotify(
+    title,
+    musicUrl,
+    setTitle,
+    setMusicUrl,
+    setShowUploadMusic 
+  );
 
-export default HomePage
+  return (
+    <div className="flex">
+      <Nav />
+      <div className="w-full m-5">
+        <Header setShowUploadMusic={setShowUploadMusic} />
+        <Playlist songs={songs} />
+        <PlayerControls />
+        {showUploadMusic && (
+          <UploadModal
+            title={title}
+            setTitle={setTitle}
+            musicUrl={musicUrl}
+            setMusicUrl={setMusicUrl}
+            newMusic={newMusic}
+            setShowUploadMusic={setShowUploadMusic}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
